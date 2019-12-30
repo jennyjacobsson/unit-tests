576448e69229a832ad35a5c9227dec775ee47dc4
Jenny Jacobsson <enni@oans.net>
2019-12-30 22:13:36 +0100

    test 1-7 fixed

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,15 @@
-export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
+export const isEmpty = stringArrayOrObject => {
+  const type = typeof stringArrayOrObject;
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  if (type === "string") {
+    return stringArrayOrObject === "";
   }
 
-  return false
-}
+  if (type === "array") {
+    return stringArrayOrObject.lenght === 0;
+  }
+
+  if (type === "object") {
+    return Object.entries(stringArrayOrObject).length === 0;
+  }
+};
